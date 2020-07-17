3be5c0b581aec08925823356d9c38d0d5faf8cdc
Arocmd9 <akanderuben17@gmail.com>
2020-07-17 09:05:26 +0100

    fourth  commit

--- mongo_db.js
+++
@@ -1,8 +0,0 @@
-let MongoClient = require('mongodb').MongoClient;
-let url = "mongodb://localhost:27017/food-quarter";
-
-MongoClient.connect(url, (err,db)=>{
-    if(err) throw err;
-    console.log("Database created successfully!");
-    db.close()
-})
